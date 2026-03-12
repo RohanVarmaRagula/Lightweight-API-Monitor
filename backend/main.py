ef41afe482ac91ecdc8c8151ff4bb1464a5a56ec
RohanVarmaRagula <ragularohanvarma@gmail.com>
2026-03-13 02:02:06 +0530

    Adding deployment path to CORS Origins

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "https://lightweight-api-monitor-one.vercel.app/"
 ]
 
 app.add_middleware(
